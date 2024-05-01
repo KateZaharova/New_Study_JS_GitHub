5d82abfd3dfba7a10a810d07fdeca1f1d3abff8a
Kate Zaharova <15071981ktz@gmail.com>
2024-05-01 22:23:11 +0300

    final tasks of modul 2

--- js_script1.js
+++ js_script1.js
@@ -796,7 +796,7 @@ function includes(array, value) {
     // Change code above this line
   }
 console.log(includes[1, 2, 3, 4, 5], 17);
-*/
+
 
 function checkAge(age) {
  
@@ -805,11 +805,348 @@ function checkAge(age) {
     return "You are an adult";
   }
   return "You are a minor";
-  
- 
 }    
+console.log(checkAge(17));
+*
 
+function checkPassword(password) {
+  const ADMIN_PASSWORD = "jqueryismyjam";
+  // Change code below this line
+
+  if (password === ADMIN_PASSWORD) {
+   return "Welcome!";
+  } 
+    return "Access denied, wrong password!";
+  
+  // Change code above this line
+}
+
+console.log(checkPassword("jqueryismyjam"));
+
+
+function checkStorage(available, ordered) {
+  // Change code below this line
+ 
+  if (ordered === 0){
+    return "Your order is empty!";
+  }
+
+    if (ordered > available){
+    return "Your order is too large, not enough goods in stock!";
+  } 
+
+  return "The order is accepted, our manager will contact you";  
+  // Change code above this line
+}
+console.log(checkStorage(100, 0));
+
+
+const fruits = ["apple", "peach", "pear", "banana"];
+
+// Change code below this line
+const lastElementIndex = fruits.length-1
+console.log(lastElementIndex);
+const lastElement = fruits[lastElementIndex];
+console.log(lastElement);
+
+
+function getExtremeElements(array) {
+  // Change code below this line
+
+  const firstArrayElement = array[0];
+  console.log(firstArrayElement);
+  const lastArrayElement = array[array.length-1];
+  console.log(lastArrayElement);
+  return array = [firstArrayElement, lastArrayElement];
+
+  // Change code above this line
+}
+
+console.log(getExtremeElements(["apple", "peach", "pear", "banana"]));
+
+
+function splitMessage(message, delimiter) {
+  let words;
+  // Change code below this line
+  words = message.split(delimiter);
+  
+  // Change code above this line
+  return words;
+}
+console.log(splitMessage("Mango hurries to the train", " "));
+
+
+function calculateEngravingPrice(message, pricePerWord) {
+   // Change code below this line
+  let words = '';
+
+  // сделать массив элементов, разделить слова на отд.индексы
+
+  words = message.split(' ');
+
+  console.log(words);
+
+  // посчитать длину масива
+  let wordsLength = words.length;
+  console.log(wordsLength);
+  // умножить цену на кол-во слов=длина масива
+
+  let totalPricePerWords = pricePerWord * wordsLength
+  
+  return totalPricePerWords;
+   // Change code above this line
+}
+
+console.log(calculateEngravingPrice("Web-development is creative work", 40));
+
+
+function makeStringFromArray(array, delimiter) {
+  let string;
+  // Change code below this line
+
+  string = array.join(delimiter);
+
+  // Change code above this line
+  return string;
+}
+console.log(makeStringFromArray(["top", "picks", "for", "you"], "_"));
+
+
+function slugify(title) {
+  // Change code below this line
+
+  let slug = title.toLowerCase().split(" ").join("-");
+  
+  return slug;
+
+  // Change code above this line
+}
+
+console.log(slugify("Arrays for begginers"));
+
+function makeArray(firstArray, secondArray, maxLength) {
+  // Change code below this line
+  // объединение 2-х массивов
+  const newArray = firstArray.concat(secondArray);
+  console.log(newArray);
+  
+  //вычисление длины нового массива
+  const newArrayLength = newArray.length;
+  console.log(newArrayLength);
+  // если maxLength больше длины нового массива - обрезать новый массив
+  // если не превышает - весь новый массив.
+  if (newArrayLength>maxLength) {
+    return newArray.slice(0, maxLength);
+  }
+  return newArray;
+
+    // Change code above this line
+}
+  
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0));
+
+
+function calculateTotal(number) {
+ // Change code below this line
+  let total = 0;
+  console.log(number);
+
+  for (let i = 1; i<= number; i += 1){
+    total = total + i;
+  }
+return total;
+  // Change code above this line
+}
+
+console.log(calculateTotal(24));
+
+
+const fruits = ['apple', 'plum', 'pear', 'orange'];
+
+for (let i = 0; i<fruits.length; i+=1) { // Change this line
+  const fruit = fruits[i]; // Change this line
+  console.log(fruit);
+}
+
+function calculateTotalPrice(order) {
+  let total = 0;
+  // Change code below this line
+ 
+  for (let i = 0; i < order.length; i += 1){
+    total = total + order[i];
+   // console.log(order[i])
+  }
+  
+  // Change code above this line
+  return total;
+}
+
+console.log(calculateTotalPrice([12, 85, 37, 4]));
+
+
+function findLongestWord(string) {
+  // Change code below this line
+  //робимо масив за допомогою м-ду split, роздільник "-"
+  let stringArray = string.split(' ');
+  console.log(stringArray);
+
+  let longestWord = stringArray[0];
+  console.log(longestWord);
+
+  // перебираємо масив
+  //порівнюємо кожне слово масиву з першим, і якщо воно більше - перевизначаємо і порівнюємо далі.
+
+  for (let i = 0; i < stringArray.length; i += 1){
+    if (longestWord.length < stringArray[i].length) {
+      longestWord = stringArray[i];
+    }
+    
+  }
+  return longestWord;
+
+
+
+  // Change code above this line
+}
+
+console.log(findLongestWord("May the force be with you"));
+
+
+function createArrayOfNumbers(min, max) {
+  const numbers = [];
+  // Change code below this line
+  for (let i = min; i <= max; i += 1){
+    numbers.push(i);
+  }
+  // Change code above this line
+  return numbers;
+}
+
+console.log(createArrayOfNumbers(14, 17));
+
+
+function filterArray(numbers, value) {
+   // Change code below this line
+
+  let newArray = [];
+
+  for (let i = 0; i < numbers.length; i += 1){
+    if (numbers[i] > value) {
+      newArray.push(numbers[i]);
+    }
+  }
+  // Change code above this line
+  return newArray;
+}
+
+console.log(filterArray([12, 24, 8, 41, 76], 20));
+
+
+function checkFruit(fruit) {
+  const fruits = ["apple", "plum", "pear", "orange"];
+
+  return fruits.includes(fruit)? true: false; // Change this line
+}
+
+console.log(checkFruit("Pear"));
+
+
+function getCommonElements(array1, array2) {
+  // Change code below this line
+
+  let newArray = [];
+
+  for (let i = 0; i < array1.length; i += 1){
+    if (array2.includes(array1[i])) {
+      newArray.push(array1[i]);
+    }
+  }
+  return newArray;
+ // Change code above this line
+}
+console.log(getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]));
+
+
+
+function calculateTotalPrice(order) {
+  let total = 0;
+  // Change code below this line
+
+  for (const i of order) {
+  total += i;
+}
+
+ /* for (let i = 0; i < order.length; i += 1) {
+  }
+
+  // Change code above this line
+  return total;
+}
+console.log(calculateTotalPrice([412, 371, 94, 63, 176]))
   
 
+function filterArray(numbers, value) {
+  // Change code below this line
+  const filteredNumbers = [];
 
-console.log(checkAge(17));
+  for (const number of numbers) {
+  if (number > value) {
+      filteredNumbers.push(number);
+    };
+  };
+
+  //for (let i = 0; i < numbers.length; i += 1) {
+    //const number = numbers[i];
+
+  return filteredNumbers;
+  // Change code above this line
+}
+
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+
+
+function getEvenNumbers(start, end) {
+   // Change code below this line
+
+  let evenArray = [];
+  
+  for (let i = start; i <= end; i += 1){
+    if (i % 2 === 0) {
+      evenArray.push(i);
+    }
+  }
+  return evenArray;
+    // Change code above this line
+}
+console.log(getEvenNumbers(7, 7));
+
+
+const start = 6;
+const end = 27;
+let number;
+
+for (let i = start; i < end; i += 1) {
+  if (i % 5 === 0) {
+    number = i;
+    break;
+  }
+}
+
+console.log(number);
+*/
+
+function findNumber(start, end, divisor) {
+  // Change code below this line
+ // let number;
+
+  for (let i = start; i < end; i += 1) {
+    if (i % divisor === 0) {
+      return i
+    }
+  }
+
+ // return number;
+  // Change code above this line
+}
+
+console.log(findNumber(16, 35, 7));
