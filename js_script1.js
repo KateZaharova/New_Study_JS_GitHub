2bf58a4b62a71fba88a2c80bacb57ba12fbe5d8d
Kate Zaharova <15071981ktz@gmail.com>
2024-05-12 22:30:50 +0300

    home tasks

--- js_script1.js
+++ js_script1.js
@@ -1420,8 +1420,258 @@ const profile = {
 };
 
 showProfileInfo(profile);
+
+
+const apartment = {
+  imgUrl: "https://via.placeholder.com/640x480",
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+  tags: ["premium", "promoted", "top"],
+  owner:{
+    name:"Henry",
+    phone: "982-126-1588",
+    email: "henry.carter@aptmail.com",  
+  },
+};
+
+
+const apartment = {
+  imgUrl: "https://via.placeholder.com/640x480",
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+  tags: ["premium", "promoted", "top"],
+  owner: {
+    name: "Henry",
+    phone: "982-126-1588",
+    email: "henry.carter@aptmail.com",
+  },
+};
+
+// Change code below this line
+const ownerName = apartment.owner.name;
+const ownerPhone = apartment.owner.phone;
+const ownerEmail = apartment.owner.email;
+const numberOfTags = apartment.tags.length;
+const firstTag = apartment.tags[0];
+const lastTag = apartment.tags[apartment.tags.length-1]
+// Change code above this line
+
+
+const apartment = {
+  imgUrl: "https://via.placeholder.com/640x480",
+  descr: "Spacious apartment in the city center",
+  rating: 4.7,
+  price: 5000,
+  tags: ["premium", "promoted", "top", "trusted"],
+  owner: {
+    name: "Henry Sibola",
+    phone: "982-126-1588",
+    email: "henry.carter@aptmail.com",
+  },
+};
+
+// Change code below this line
+apartment.area = 60;
+apartment.rooms = 3;
+apartment.location={
+  country:"Jamaica",
+    city:"Kingston",
+};
+
+
+const apartment = {
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+};
+const keys = [];
+const values = [];
+// Change code below this line
+for (const key in apartment) {
+  keys.push(key);
+  values.push(apartment[key])
+};
+console.log(keys);
+console.log(values);
+
+
+function countProps(object) {
+  let propCount = 0;
+  // Change code below this line
+  for (const key in object) {
+    if (object.hasOwnProperty(key)) {
+      propCount += 1;
+    }
+}
+  // Change code above this line
+  return propCount;
+}
+
+console.log(countProps({ name: "Mango", age: 2 }));
+console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 }));
+console.log(countProps({}));
+
+
+const apartment = {
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+};
+const values = [];
+// Change code below this line
+const keys = Object.keys(apartment);
+
+for (const key of keys) {
+  values.push(apartment[key]);
+}
+console.log(values);
+console.log(keys);
+
+
+function countProps(object) {
+  // Change code below this line
+  let propCount = 0;
+
+  const keys = Object.keys(object);
+    propCount = keys.length;
+ 
+  return propCount;
+  // Change code above this line
+}
+console.log(countProps({ name: "Mango", age: 2 }));
+
+
+function countTotalSalary(salaries) {
+  let totalSalary = 0;
+  // Change code below this line
+
+  const keys = Object.keys(salaries);
+  const values = Object.values(salaries);
+
+  for (const value of values) {
+    totalSalary += value;
+}
+
+   // Change code above this line
+  return totalSalary;
+}
+console.log(countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 }));
+console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 })); // 330
+
+const books = [
+  {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    rating: 8.38,
+  },
+  {
+    title: "Beside Still Waters",
+    author: "Robert Sheckley",
+    rating: 8.51,
+  },
+  {
+    title: "The Dream of a Ridiculous Man",
+    author: "Fyodor Dostoevsky",
+    rating: 7.75,
+  },
+];
+
+for (const book of books) {
+  // Об'єкт книги
+  //console.log(book);
+  // Назва
+  console.log(book.title);
+  // Автор
+  //console.log(book.author);
+  // Рейтинг
+  //console.log(book.rating);
+}
+
+const colors = [
+  { hex: "#f44336", rgb: "244,67,54" },
+  { hex: "#2196f3", rgb: "33,150,243" },
+  { hex: "#4caf50", rgb: "76,175,80" },
+  { hex: "#ffeb3b", rgb: "255,235,59" },
+];
+
+const hexColors = [];
+const rgbColors = [];
+//const keys = Object.keys(colors);
+//console.log(keys);
+// Change code below this line
+
+for (const color of colors) {
+  hexColors.push(color.hex);
+  rgbColors.push(color.rgb);
+}
+  console.log(hexColors);
+  console.log(rgbColors)
+
+
+const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+function getProductPrice(productName) {
+  // Change code below this line
+  for (const product of products) {
+     
+    if (product.name === productName) {
+      return product.price;
+    } 
+  }
+      return "null";
+}
+  // Change code above this line
+
+console.log(getProductPrice("Radar"));
+console.log(getProductPrice("Engine"));
+console.log(getProductPrice("Scanner"));
 */
 
+const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+//const keys = [];
+//const valueArray = []
+
+function getAllPropValues(propName) {
+  // Change code below this line
+  for (const product of products) {
+
+    const keys = Object.keys(product);
+    console.log(keys[propName]);
+
+
+    const values = Object.values(product);
+    console.log(values[0]);
+
+    if (Object.values(product)) {
+     // return values;
+      
+    }
+  }
+  //return [];
+}
+  // Change code above this line
+
+
+//console.log(getAllPropValues("name")); //["Radar", "Scanner", "Droid", "Grip"])
+//console.log(getAllPropValues("quantity"));
+console.log(getAllPropValues("price"));
+
+
+
+/*
 const cart = {
   items: [],
   getItems() { 
@@ -1445,4 +1695,4 @@ cart.add({ name: "tomato", price: 110 });
 
 
 console.table(cart.getItems());
-
+*/
