228c7617362c6f2196addac522e5539ae063322c
Kate Zaharova <15071981ktz@gmail.com>
2024-05-27 22:24:26 +0300

    mod4

--- js_script4.js
+++ js_script4.js
@@ -74,7 +74,7 @@ for (let i = 0; i < numbers.length; i += 1){
 numbers.forEach(function (number, index) {
     console.log(`Index ${index}, value: ${number}.`)
 })
-*/
+
 
 function classicAdd(a, b, c) {
     return a+b+c
@@ -89,3 +89,102 @@ const hello = () => {
 }
 
 hello()
+
+
+const filter = function (array, test) {
+    const filteredArray = [];
+
+    for (const el of array) {
+
+        if (test(el)) {
+            filteredArray.push(el);
+        }
+    }
+    return filteredArray;
+}
+
+
+const r1 = filter([1,2,3,4,5,6,7,8,9], function (value){
+    return value >= 5;
+});
+console.log(r1);
+
+
+const r2 = filter([1, 2, 3, 4, 5, 6, 7, 8, 9], function (value) {
+    return value <= 4;
+});
+console.log(r2);
+
+
+const filter = function (array, test) {
+    for (const a of array) {
+        if (test(a)) {
+            return `We have found ${a}!`
+        }
+    }
+};
+
+const arguments1 = filter(["apple", "banana", "orange"], function (item) {
+    return item.includes("orange");
+})
+
+console.log(arguments1);
+
+
+const filter = function (array, start) {
+    
+    const positiveArray = []
+
+    for (const a of array) {
+        if (start(a)%2===0) {
+                positiveArray.push(a)
+            }
+    }
+    return positiveArray;
+};
+
+const test = filter([1, 2, 3, 4, 5, 6, 7, 98], function (value) {
+    return value ;   
+})
+
+console.log(test);
+
+const test2 = filter([4, 0, -4, 10, -3, 15], function (value) {
+    return value ;   
+})
+
+console.log(test2);
+
+const filter = function (art, start) {
+    let total = 0;
+    for (const a of art) {
+        //console.log(a)
+        if (start(a)){
+            total += a;   
+        }
+    }
+    return total;
+}
+
+const result = filter([1, 2, 3, 4, 5, 6, 7, 8, 9], function (value){
+    return value >= 5;
+}
+);
+console.log(result);*/
+
+const letItBe = function (array, cb) {
+    let newArray = [];
+    for (const a of array) {
+        console.log(a)
+        if (cb(a)) {
+            newArray.push(a);
+        }
+    }
+    return newArray;
+};
+
+const result = letItBe([15,25,24,12,13,0,18,19,55], function (element) {
+    return element % 2 === 0;
+})
+
+console.log(result);
